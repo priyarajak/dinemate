8ca532f6133997e549833ebe5e4ffdbc325f4a24
Priya <JennyPriya@Priyas-MacBook-Pro.local>
2025-09-09 19:45:45 +0530

    bug fixes 4

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
     dispatch(loadOrdersFromStorage());
   }, [dispatch]);
   return (
-    <Router basename="/dinemate" >
+    <Router basename="/dinemate">
       <Routes>
         <Route path="/" element={<StartPage />} />
         <Route path="/admin-login" element={<AdminPage />} />
